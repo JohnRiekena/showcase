7938faa87d9ea5a444b2039244d6cac83f178b70
John Riekena <john@johns-mbp.lan>
2024-06-11 00:50:22 -0500

    added books with active storage

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -65,6 +65,6 @@ class BooksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def book_params
-      params.require(:book).permit(:title, :page_count, :genre)
+      params.require(:book).permit(:title, :page_count, :genre, :image)
     end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
+  has_one_attached :image
+
   # allows for handy activerecord queries (i.e. Book.mystery, book_instance.mystery?, etc)
   enum genre: {
     uncategorized: 'Uncategorized',
